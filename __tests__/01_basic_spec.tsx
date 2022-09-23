a0b054c6e79c3ff5208bbebb410778a5c5292192
daishi <daishi@axlight.com>
2022-09-23 11:35:29 +0900

    feat: infinite query

--- __tests__/01_basic_spec.tsx
+++ __tests__/01_basic_spec.tsx
@@ -1,7 +1,13 @@
-import { atomsWithTanstackQuery } from '../src/index'
+import {
+  atomsWithTanstackInfiniteQuery,
+  atomsWithTanstackQuery,
+  queryClientAtom,
+} from '../src/index'
 
 describe('basic spec', () => {
   it('should export functions', () => {
+    expect(queryClientAtom).toBeDefined()
     expect(atomsWithTanstackQuery).toBeDefined()
+    expect(atomsWithTanstackInfiniteQuery).toBeDefined()
   })
 })
---
+++ src/atomsWithTanstackInfiniteQuery.ts
@@ -0,0 +1,142 @@
+import { InfiniteQueryObserver, QueryClient } from '@tanstack/query-core'
+import type {
+  InfiniteQueryObserverOptions,
+  InfiniteQueryObserverResult,
+  QueryKey,
+  ResetOptions,
+} from '@tanstack/query-core'
+import { atom } from 'jotai'
+import type { Getter } from 'jotai'
+import { atomWithDefault, atomWithObservable } from 'jotai/utils'
+import { queryClientAtom } from './queryClientAtom'
+
+type Action = {
+  type: 'refetch'
+  force?: boolean
+  options?: ResetOptions
+}
+
+export function atomsWithTanstackInfiniteQuery<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey
+>(
+  getOptions: (
+    get: Getter
+  ) => InfiniteQueryObserverOptions<
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryData,
+    TQueryKey
+  >,
+  getQueryClient: (get: Getter) => QueryClient = (get) => get(queryClientAtom)
+) {
+  type Result = InfiniteQueryObserverResult<TData, TError>
+
+  const observerCacheAtom = atom(
+    () =>
+      new WeakMap<
+        QueryClient,
+        InfiniteQueryObserver<
+          TQueryFnData,
+          TError,
+          TData,
+          TQueryData,
+          TQueryKey
+        >
+      >()
+  )
+
+  const refreshAtom = atom(0)
+
+  const observerAtom = atom((get) => {
+    get(refreshAtom)
+    const queryClient = getQueryClient(get)
+    const options = getOptions(get)
+    const observerCache = get(observerCacheAtom)
+    let observer = observerCache.get(queryClient)
+    if (observer) {
+      observer.setOptions(options)
+    } else {
+      observer = new InfiniteQueryObserver(queryClient, options)
+      observerCache.set(queryClient, observer)
+    }
+    return observer
+  })
+
+  const resultAtom = atomWithDefault((get) => {
+    const observer = get(observerAtom)
+    return observer.getCurrentResult() as Result
+  })
+
+  const baseStatusAtom = atom(
+    (get) => get(resultAtom),
+    (get, set, returnUnsubscribe: (unsubscribe: () => void) => void) => {
+      const observer = get(observerAtom)
+      const unsubscribe = observer.subscribe((result) => {
+        set(resultAtom, result)
+      })
+      returnUnsubscribe(unsubscribe)
+    }
+  )
+  baseStatusAtom.onMount = (initialize) => {
+    let unsub: (() => void) | undefined
+    initialize((unsubscribe) => {
+      unsub = unsubscribe
+    })
+    return unsub
+  }
+
+  const statusAtom = atom(
+    (get) => get(baseStatusAtom),
+    (get, set, action: Action) => {
+      if (action.type === 'refetch') {
+        const observer = get(observerAtom)
+        if (action.force) {
+          observer.remove()
+          const queryClient = getQueryClient(get)
+          const observerCache = get(observerCacheAtom)
+          observerCache.delete(queryClient)
+          set(refreshAtom, (c) => c + 1)
+          return
+        }
+        return observer.refetch(action.options).then(() => {})
+      }
+    }
+  )
+
+  const baseDataAtom = atomWithObservable((get) => {
+    const observer = get(observerAtom)
+    const observable = {
+      subscribe: (
+        arg: { next: (result: Result) => void } | ((result: Result) => void)
+      ) => {
+        const callback = (result: Result) => {
+          if (result.error || result.data !== undefined) {
+            ;(typeof arg === 'function' ? arg : arg.next)(result)
+          }
+        }
+        const unsubscribe = observer.subscribe(callback)
+        callback(observer.getCurrentResult() as Result)
+        return { unsubscribe }
+      },
+    }
+    return observable
+  })
+
+  const dataAtom = atom(
+    (get) => {
+      const baseData = get(baseDataAtom)
+      if (baseData.error) {
+        throw baseData.error
+      }
+      return baseData.data as TData
+    },
+    (_get, set, action: Action) => set(statusAtom, action)
+  )
+
+  return [dataAtom, statusAtom] as const
+}
