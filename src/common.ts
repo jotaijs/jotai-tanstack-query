8614d200f69ee9ffce002a752edca3cf8cb8cafd
daishi <daishi@axlight.com>
2022-09-24 18:25:05 +0900

    wip: pending callbacks cache

--- src/common.ts
+++ src/common.ts
@@ -83,9 +83,23 @@ export const createAtoms = <
     }
   )
 
+  const pendingCallbacksCache = new WeakMap<
+    Observer,
+    Set<(result: Result) => void>
+  >()
+  const getPendingCallbacks = (observer: Observer) => {
+    let pendingCallbacks = pendingCallbacksCache.get(observer)
+    if (!pendingCallbacks) {
+      pendingCallbacks = new Set()
+      pendingCallbacksCache.set(observer, pendingCallbacks)
+    }
+    return pendingCallbacks
+  }
+
   const baseDataAtom = atom((get) => {
     getOptions(get) // re-create observable when options change
     const observer = get(observerAtom)
+    const pendingCallbacks = getPendingCallbacks(observer)
     const observable = {
       subscribe: (
         arg: { next: (result: Result) => void } | ((result: Result) => void)
@@ -96,20 +110,21 @@ export const createAtoms = <
             (result.isError && !isCancelledError(result.error))
           ) {
             ;(typeof arg === 'function' ? arg : arg.next)(result)
+            pendingCallbacks.forEach((cb) => {
+              if (cb !== callback) {
+                cb(result)
+              }
+            })
+            pendingCallbacks.clear()
           }
         }
+        pendingCallbacks.add(callback)
         const unsubscribe = observer.subscribe(callback)
         callback(observer.getCurrentResult())
         return { unsubscribe }
       },
     }
-    const currentResult = observer.getCurrentResult()
-    const options =
-      (currentResult.isSuccess && currentResult.data !== undefined) ||
-      (currentResult.isError && !isCancelledError(currentResult.error))
-        ? { initialValue: currentResult }
-        : {}
-    const resultAtom = atomWithObservable(() => observable, options)
+    const resultAtom = atomWithObservable(() => observable)
     return resultAtom
   })
 
