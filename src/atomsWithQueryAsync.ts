6e70cb3dab7a233bf4ea483c9d5fea0e135d6cc2
Daryl Castro <darylcastro67@gmail.com>
2023-03-22 23:40:57 +0100

    feat: atomsWithQueryAsync, plus example

---
+++ src/atomsWithQueryAsync.ts
@@ -0,0 +1,58 @@
+import type { Getter, ExtractAtomValue, WritableAtom } from 'jotai'
+import type { QueryKey, QueryObserverOptions } from '@tanstack/query-core'
+
+import { atom } from 'jotai'
+
+import { atomsWithQuery } from './atomsWithQuery'
+
+type ExtractAtomArgs<AtomType> = AtomType extends WritableAtom<
+  any,
+  infer Args,
+  any
+>
+  ? Args
+  : never
+
+export function atomsWithQueryAsync<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey
+>(
+  getOptions: (
+    get: Getter
+  ) => Promise<
+    QueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>
+  >
+) {
+  const atomsAtom = atom(async (get) => {
+    const options = await getOptions(get)
+    return atomsWithQuery(() => options)
+  })
+
+  const dataAtom = atom(
+    async (get) => get((await get(atomsAtom))[0]),
+    async (
+      get,
+      set,
+      ...args: ExtractAtomArgs<Awaited<ExtractAtomValue<typeof atomsAtom>>[0]>
+    ) => {
+      const a = (await get(atomsAtom))[0]
+      return set(a, ...args)
+    }
+  )
+
+  const statusAtom = atom(
+    async (get) => get((await get(atomsAtom))[1]),
+    async (
+      get,
+      set,
+      ...args: ExtractAtomArgs<Awaited<ExtractAtomValue<typeof atomsAtom>>[1]>
+    ) => {
+      const a = (await get(atomsAtom))[1]
+      return set(a, ...args)
+    }
+  )
+  return [dataAtom, statusAtom] as const
+}
