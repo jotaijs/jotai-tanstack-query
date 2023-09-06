e124e35297f9d7a42e344e9f42ce0dc474f10756
Mohammad Bagher Abiat <zorofight94@gmail.com>
2023-09-06 09:32:54 +0330

    fix

--- __tests__/atomWithMutation.tsx
+++ __tests__/atomWithMutation.tsx
@@ -3,70 +3,7 @@ import { fireEvent, render } from '@testing-library/react'
 import { useAtom } from 'jotai/react'
 import { atomsWithMutation } from '../src/index'
 
-// it('status should change', async () => {
-//   const idAtom = atom(undefined as number | undefined)
-
-//   let resolve: (() => void) | undefined
-//   const [, statusAtom] = atomsWithQuery((get) => ({
-//     queryKey: ['users', get(idAtom)],
-//     queryFn: async ({ queryKey: [, id] }) => {
-//       await new Promise<void>((r) => {
-//         resolve = r
-//       })
-//       return { id }
-//     },
-//     // using as a dependent query
-//     enabled: Boolean(get(idAtom)),
-//   }))
-
-//   const DataFromStatusAtom = () => {
-//     const [status] = useAtom(statusAtom)
-//     const statusMessage = status.isInitialLoading
-//       ? 'initial loading'
-//       : status.status
-//     return (
-//       <div>
-//         <div>status: {statusMessage}</div>
-//         <div>data: {JSON.stringify(status.data)}</div>
-//       </div>
-//     )
-//   }
-
-//   const Controls = () => {
-//     const [id, setId] = useAtom(idAtom)
-//     return (
-//       <div>
-//         ID: {id}{' '}
-//         <button type="button" onClick={() => setId((c = 0) => c + 1)}>
-//           Next
-//         </button>
-//       </div>
-//     )
-//   }
-
-//   const App = () => (
-//     <>
-//       <Controls />
-//       <DataFromStatusAtom />
-//     </>
-//   )
-
-//   const { findByText, getByText } = render(<App />)
-
-//   await findByText('status: loading')
-
-//   fireEvent.click(getByText('Next'))
-//   await findByText('status: initial loading')
-//   resolve?.()
-//   await findByText('status: success')
-
-//   fireEvent.click(getByText('Next'))
-//   await findByText('status: initial loading')
-//   resolve?.()
-//   await findByText('status: success')
-// })
-
-it('the mutation atom should be refreshed on unmount (#2060)', async () => {
+it('atomsWithMutation should be refreshed on unmount (#2060)', async () => {
   let resolve: (() => void) | undefined
   const [, testAtom] = atomsWithMutation<number, number, number, number>(
     () => ({
