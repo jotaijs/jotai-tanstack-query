82dac9a18d0c8e99e409869b8c7fda0cde3204ac
grzesiek-ds <grzegorz@airspace-intelligence.com>
2024-03-12 21:50:58 +0100

    allow to auto-reset error in atomWithSuspenseQuery

--- src/utils.ts
+++ src/utils.ts
@@ -80,3 +80,6 @@ export const ensureStaleTime = (
 
   return defaultedOptions
 }
+
+export const wait = (timeout: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, timeout))
