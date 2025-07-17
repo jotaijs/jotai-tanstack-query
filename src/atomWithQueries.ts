cf468bd153aac7567d06bd0793886cf31fe3e690
Alex Yang <himself65@outlook.com>
2025-07-17 15:54:33 -0700

    fix: type fix

--- src/atomWithQueries.ts
+++ src/atomWithQueries.ts
@@ -1,27 +1,53 @@
-import { QueryClient, QueryObserver } from '@tanstack/query-core'
+import {
+  type DefaultError,
+  QueryClient,
+  type QueryKey,
+  QueryObserver,
+} from '@tanstack/query-core'
 import { Getter, WritableAtom, atom } from 'jotai'
 import { baseAtomWithQuery } from './baseAtomWithQuery'
 import { queryClientAtom } from './queryClientAtom'
 import { AtomWithQueryOptions, AtomWithQueryResult } from './types'
 
-export function atomWithQueries<TCombinedResult>(
+export function atomWithQueries<
+  TCombinedResult,
+  TQueryFnData = unknown,
+  TError = DefaultError,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+>(
   {
     queries,
     combine,
   }: {
-    queries: Array<(get: Getter) => AtomWithQueryOptions>
-    combine: (results: AtomWithQueryResult[]) => TCombinedResult
+    queries: Array<
+      (
+        get: Getter
+      ) => AtomWithQueryOptions<TQueryFnData, TError, TData, TQueryKey>
+    >
+    combine: (
+      results: AtomWithQueryResult<TQueryFnData, TError>[]
+    ) => TCombinedResult
   },
   getQueryClient?: (get: Getter) => QueryClient
 ): WritableAtom<TCombinedResult, [], void>
-export function atomWithQueries(
+export function atomWithQueries<
+  TQueryFnData = unknown,
+  TError = DefaultError,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+>(
   {
     queries,
   }: {
-    queries: Array<(get: Getter) => AtomWithQueryOptions>
+    queries: Array<
+      (
+        get: Getter
+      ) => AtomWithQueryOptions<TQueryFnData, TError, TData, TQueryKey>
+    >
   },
   getQueryClient?: (get: Getter) => QueryClient
-): Array<WritableAtom<AtomWithQueryResult, [], void>>
+): Array<WritableAtom<AtomWithQueryResult<TQueryFnData, TError>, [], void>>
 
 export function atomWithQueries(
   {
